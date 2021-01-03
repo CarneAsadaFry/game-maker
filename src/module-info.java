e28804b159ec647a1174b42bea9ec0428f402fc4
CarneAsadaFry <ntausik@berkeley.edu>
2021-01-02 20:06:45 -0800

    Update for IntelliJ

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module AlgorithmsFinal {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.desktop;
+
+    exports application;
+    opens application to javafx.graphics;
+}
